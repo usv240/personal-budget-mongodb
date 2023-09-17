5546aea96bd17732bba6d9d303a24aa035e6af37
usv240 <ujwalsureshv@gmail.com>
2023-09-17 11:30:56 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,36 @@
+const exp = require('constants');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget ={ 
+    myBudget: [
+    {
+        title: 'Eat out',
+        budget: 25
+    },
+    {
+        title: 'Rent',
+        budget: 375
+    },
+    {
+        title: 'grocery',
+        budget: 110
+    },
+]
+};
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
