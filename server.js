e7d7633442d9e0b89a14917432f1ea27b4aed926
usv240 <ujwalsureshv@gmail.com>
2023-09-17 12:39:16 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const exp = require('constants');
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use('/', express.static('public'));
+app.use('/bug',express.static('budget_data.json'));
 
 const budget ={ 
     myBudget: [
@@ -26,11 +28,19 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
+
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('budget_data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while reading budget data.' });
+        } else {
+            const budgetData = JSON.parse(data);
+            res.json(budgetData);
+        }
+    });
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
