f209215e66c42a6908b4aa046fc1806b3ad0a38b
usv240 <ujwalsureshv@gmail.com>
2023-09-24 15:14:21 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const exp = require('constants');
+// Budget API
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 
-app.use('/', express.static('public'));
-app.use('/bug',express.static('budget_data.json'));
+app.use(cors()); // Use cors() as a middleware function.
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+// app.use('/', express.static('public'));
+app.use('/bug', express.static('budget_data.json'));
 
+// app.get('/hello', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.get('/budget', (req, res) => {
     fs.readFile('budget_data.json', 'utf8', (err, data) => {
@@ -19,11 +21,11 @@ app.get('/budget', (req, res) => {
             res.status(500).json({ error: 'An error occurred while reading budget data.' });
         } else {
             const budgetData = JSON.parse(data);
-            res.render('index.html', { budgetData }); // Pass budgetData to the HTML file
+            res.json(budgetData);
         }
     });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
